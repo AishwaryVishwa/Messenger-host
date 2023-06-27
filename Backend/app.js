936539bab9152cb07926c3f5bda9f1449d14ea12
AishwaryVishwa <aishwaryvishwakarma11@gmail.com>
2023-06-27 12:26:35 +0530

    endpoint

--- Backend/app.js
+++ Backend/app.js
@@ -23,7 +23,7 @@ app.use(require('./Router/auth'))
 const io=new Server(server,{
     pingTimeout:60000,
     cors:{
-        origin:"http://chat-application-frontend-h29qamhyj-aishwaryvishwa.vercel.app"
+        origin:"http://http://localhost:3000"
     }
 })
 
