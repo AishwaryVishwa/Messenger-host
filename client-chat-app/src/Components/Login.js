c59f33b246a7aba8bd20614b888b86fab8ddd4d4
AishwaryVishwa <aishwaryvishwakarma11@gmail.com>
2023-06-28 23:19:05 +0530

    login

--- client-chat-app/src/Components/Login.js
+++ client-chat-app/src/Components/Login.js
@@ -17,7 +17,7 @@ function Login() {
         password: passw
     }
     const loginHandler = async () => {
-        const res = await axios.post('/login', loginData)
+        const res = await axios.post('https://chat-backend-otkw.onrender.com/login', loginData)
         if (res.status === 200) {
             window.alert('login successful')
             console.log(res.data);
